ba147130a7eec7f6afd8055c1f14d493345bf028
ScheglyakStas <93485732+ScheglyakStas@users.noreply.github.com>
2022-01-19 19:14:23 +0300

    message

---
+++ random/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    long long n,d,x,i;
+    cin>>n>>d;
+    ofstream cout("input.txt");
+    //Числа генерируются в диапазоне от -d до d включительно.
+    cout<<n<<endl;
+    for(i=0; i<n; i++)
+    {
+        x=rand()%(2*d+1)-d;
+        cout<<x<<endl;
+    }
+    return 0;
+}
