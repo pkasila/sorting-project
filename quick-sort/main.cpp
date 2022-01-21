5e92f9ce146cbe572155a083228fecf7e72dedd3
Pavel Kasila <pavel.kasila@gmail.com>
2022-01-21 14:22:52 +0300

    Update to confront coding style

--- quick-sort/main.cpp
+++ quick-sort/main.cpp
@@ -3,40 +3,39 @@
 #include <fstream>
 #include <ctime>
 
-void quicksort(std::vector < int > &mas, int first, int last){
+void quicksort(std::vector<int> &mas, int first, int last) {
     int mid;
     int f = first;
     int l = last;
     mid = mas[(f + l) / 2];
-    do{
+    do {
         while (mas[f] < mid) f++;
         while (mas[l] > mid) l--;
-        if (f <= l){
+        if (f <= l) {
             int temp = mas[f];
             mas[f] = mas[l];
             mas[l] = temp;
             f++;
             l--;
         }
-    }while (f < l);
+    } while (f < l);
     if (first < l) quicksort(mas, first, l);
     if (f < last) quicksort(mas, f, last);
 }
 
-int main()
-{
-    std::ifstream fin ("input.txt");
-    std::ofstream fout ("quick-sort.txt");
+int main() {
+    std::ifstream fin("input.txt");
+    std::ofstream fout("quick-sort.txt");
     int numbers;
     fin >> numbers;
-    std::vector < int > Massive(numbers);
-    for(int i=0; i<numbers; i++)
+    std::vector<int> Massive(numbers);
+    for (int i = 0; i < numbers; i++)
         fin >> Massive[i];
     int t1 = clock();
-    quicksort(Massive, 0, numbers-1);
+    quicksort(Massive, 0, numbers - 1);
     int t2 = clock();
-    fout << t2-t1 << std::endl;
-    for(auto i : Massive)
+    fout << t2 - t1 << std::endl;
+    for (auto i: Massive)
         fout << i << ' ';
     return 0;
 }
--- swap-sort/main.cpp
+++ swap-sort/main.cpp
@@ -2,36 +2,32 @@
 #include <fstream>
 #include <vector>
 #include <ctime>
+
 using namespace std;
-int main()
-{
-    long long i,n,j,t,t1,t2;
+
+int main() {
+    long long i, n, j, t, t1, t2;
     ifstream cin("input.txt");
     ofstream cout("output.txt");
-    cin>>n;
-    cout<<n<<endl;
-    vector<long long>S(n);
-    for(i=0; i<n; i++)
-    {
-        cin>>S[i];
+    cin >> n;
+    cout << n << endl;
+    vector<long long> S(n);
+    for (i = 0; i < n; i++) {
+        cin >> S[i];
     }
-    t1=clock();
-    for(i=1; i<n; i++)
-    {
-        for(j=0; j<n-i; j++)
-        {
-            if(S[j]>S[j+1])
-            {
-                swap(S[j],S[j+1]);
+    t1 = clock();
+    for (i = 1; i < n; i++) {
+        for (j = 0; j < n - i; j++) {
+            if (S[j] > S[j + 1]) {
+                swap(S[j], S[j + 1]);
             }
         }
     }
-    t2=clock();
-    t=t2-t1;
-    cout<<t<<" ms"<<endl;
-    for(i=0; i<n; i++)
-    {
-        cout<<S[i]<<' ';
+    t2 = clock();
+    t = t2 - t1;
+    cout << t << " ms" << endl;
+    for (i = 0; i < n; i++) {
+        cout << S[i] << ' ';
     }
     return 0;
 }
