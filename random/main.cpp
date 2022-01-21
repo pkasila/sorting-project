2e3bfb079c7b3d8d622ff4b3ea2307980e65aa4d
Pavel Kasila <pavel.kasila@gmail.com>
2022-01-21 14:56:10 +0300

    Add scripts/gen_sources.sh (fix errors in random project)

--- random/main.cpp
+++ random/main.cpp
@@ -1,20 +1,47 @@
 #include <iostream>
 #include <fstream>
-#include <stdlib.h>
-#include <time.h>
 
 using namespace std;
 
-int main() {
-    srand(time(NULL));
-    long long n, d, x, i;
-    cin >> n >> d;
-    ofstream cout("input.txt");
-    //Числа генерируются в диапазоне от -d до d включительно.
-    cout << n << endl;
+static int find_keyword(int argc, char *argv[], const char *keyword) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], keyword) == 0) return i;
+    }
+    return -1;
+}
+
+int main(int argc, char *argv[]) {
+    const int nIndex = find_keyword(argc, argv, "-n");
+    if (nIndex < 0) {
+        printf("No -n keyword found!  Exiting!\n");
+        exit(10);
+    }
+
+    const int minIndex = find_keyword(argc, argv, "-min");
+    if (minIndex < 0) {
+        printf("No -min keyword found!  Exiting!\n");
+        exit(10);
+    }
+
+    const int maxIndex = find_keyword(argc, argv, "-max");
+    if (maxIndex < 0) {
+        printf("No -max keyword found!  Exiting!\n");
+        exit(10);
+    }
+
+    const int outIndex = find_keyword(argc, argv, "-out");
+    if (maxIndex < 0) {
+        printf("No -out keyword found!  Exiting!\n");
+        exit(10);
+    }
+
+    long long n = stol(argv[nIndex + 1]),
+            min = stol(argv[minIndex + 1]), max = stol(argv[maxIndex + 1]), x, i;
+    ofstream fout(argv[outIndex + 1]);
+    fout << n << endl;
     for (i = 0; i < n; i++) {
-        x = rand() % (2 * d + 1) - d;
-        cout << x << endl;
+        x = rand() % (max - min) + min;
+        fout << x << endl;
     }
     return 0;
 }
