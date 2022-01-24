bda423bc72023fb86a357cf8738a89437724c72b
Pavel Kasila <pavel.kasila@gmail.com>
2022-01-24 23:14:30 +0300

    Add all stuff needed for study data generation: #9

---
+++ inverse/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+static int find_keyword(int argc, char *argv[], const char *keyword) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], keyword) == 0) return i;
+    }
+    return -1;
+}
+
+int main(int argc, char *argv[]) {
+    const int inIndex = find_keyword(argc, argv, "-in");
+    if (inIndex < 0) {
+        printf("No -in keyword found!  Exiting!\n");
+        exit(10);
+    }
+
+    const int outIndex = find_keyword(argc, argv, "-out");
+    if (outIndex < 0) {
+        printf("No -out keyword found!  Exiting!\n");
+        exit(10);
+    }
+
+    std::ifstream fin(argv[inIndex + 1]);
+    std::ofstream fout(argv[outIndex + 1]);
+
+    int n;
+    fin >> n;
+    std::vector<int> v;
+
+    while(!fin.eof()) {
+        int i;
+        fin >> i;
+        v.emplace_back(i);
+    }
+
+    std::reverse(v.begin(), v.end());
+
+    fout << v.size() << std::endl;
+    for (auto i: v)
+        fout << i << ' ';
+}
--- scripts/sort.sh
+++ scripts/sort.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 mkdir -p data/out
+mkdir -p data/inversed/source
+mkdir -p data/inversed/out
 
-declare -a sortings=("insertion-sort" "merge-sort" "quick-sort" "selection-sort" "swap-sort")
-declare -a files_a=("20k_100" "20k_10000" "40k_100" "40k_10000" "100k_100" "100k_10000")
+declare -a sortings=("selection-sort" "swap-sort" "insertion-sort")
+declare -a files_a=("20k_100" "20k_10000" "40k_100" "40k_10000")
 
 for i in "${sortings[@]}"
 do
@@ -12,5 +14,54 @@ do
    do
       echo " - $j"
       "./$i/$i" -in "data/source/$j.txt" -out "data/out/$i-$j.txt"
+   done
+done
+
+# worst case scenario
+for j in "${files_a[@]}"
+do
+  echo " - inversing $j"
+  ./inverse/inverse -in "data/out/swap-sort-$j.txt" -out "data/inversed/source/$j.txt"
+done
+
+for i in "${sortings[@]}"
+do
+   echo "* $i"
+   for j in "${files_a[@]}"
+   do
+      echo " - $j (inversed)"
+      "./$i/$i" -in "data/inversed/source/$j.txt" -out "data/inversed/out/$i-$j.txt"
+   done
+done
+
+# task 2
+
+declare -a sortings=("merge-sort" "quick-sort" "std-sort")
+declare -a files_a=("1m_100" "1m_10000" "2m_100" "2m_10000")
+
+for i in "${sortings[@]}"
+do
+   echo "* $i"
+   for j in "${files_a[@]}"
+   do
+      echo " - $j"
+      "./$i/$i" -in "data/source/$j.txt" -out "data/out/$i-$j.txt"
+   done
+done
+
+# worst case scenario
+for j in "${files_a[@]}"
+do
+  echo " - inversing $j"
+  ./inverse/inverse -in "data/out/std-sort-$j.txt" -out "data/inversed/source/$j.txt"
+done
+
+for i in "${sortings[@]}"
+do
+   echo "* $i"
+   for j in "${files_a[@]}"
+   do
+      echo " - $j (inversed)"
+      "./$i/$i" -in "data/inversed/source/$j.txt" -out "data/inversed/out/$i-$j.txt"
    done
 done
